fe947ce2144faf0850022eb0aceea4c688147f5f
revxi <138651309+revxi@users.noreply.github.com>
2025-07-21 15:08:27 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Operating System Simulator</h1>
+      <p>Coming soon: CPU Scheduling and Memory Management Algorithms</p>
+    </div>
+  );
+}
+
+export default App;
